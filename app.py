c574c5891333cd61cfb30172d780d3ee138360f6
Ignacio Arriagada <iarriagada@gemini.edu>
2023-05-14 16:13:54 -0700

    Cleaned up and added comments to app script

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# IP for the GWS IOC
 os.environ["EPICS_CA_ADDR_LIST"] = "172.17.2.45"
 
 gws_key = {
@@ -22,16 +23,6 @@ gws_api_dict = {
     "wind_speed":{"value":None, "timestamp":None, "unit":"m/s"},
     "wind_dir":{"value":None, "timestamp":None, "unit":"deg"},
 }
-
-greetings = [
-    {"id":1, "name":"Hola", "lang":"espanol"},
-    {"id":2, "name":"Hello", "lang":"english"},
-    {"id":3, "name":"Kipa", "lang":"chileno"}
-]
-
-@app.get("/greetings")
-def get_greetings():
-    return jsonify(greetings)
 
 @app.get("/gws_values")
 def get_gws_values():
@@ -50,12 +41,23 @@ def connect_epics_chans(chan_dict):
 
 def mon_epics_chans(epics_chans, epics_key, api_dict):
     for c in epics_chans:
+        # Initialize the values for each PV, before starting the monitor
         api_dict[epics_key[c]]['value'] = epics_chans[c].value
+        # Add the monitor
         epics_chans[c].add_callback(on_change,
                                     key=epics_key,
                                     vals=api_dict)
 
 def on_change(pvname=None, value=None, timestamp=None, **kw):
+    '''
+    This function is meant to be used as a callback for an EPICS monitor. The
+    function receives a dictionary with the key between PV name and EPICS
+    channel name. Then it updates the values and timestamps accordingly.
+    The variables that should be passed as kw are:
+        - key: This is the map of EPICS chan / PV name
+        - vals: This is the dictionary that will hold the PVs value and
+        timestamp
+    '''
     kw['vals'][kw['key'][pvname]]['value'] = value
     kw['vals'][kw['key'][pvname]]['timestamp'] = timestamp
 
